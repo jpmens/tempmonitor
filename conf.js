993c7bb26838809b9086b0f96dba57e6b9aa0e5c
Jan-Piet Mens <jpmens@gmail.com>
2015-04-11 19:22:04 +0200

    publish after LOADays

---
+++ conf.js
@@ -0,0 +1,6 @@
+var websocketserver = '10.0.69.162';
+var websocketport   = 9001;
+var topic = 'clients/arduino/10.0.69.105/#';
+
+var ledtopic = 'clients/arduino/led'	// Web PUBlishes to ledtopic + "/cmd"
+
--- runWSS.sh
+++
@@ -1,1 +0,0 @@
-/Users/jpm/Auto/pubgit/MQTT/WSS/WSS_static_osx --websocketPort 18884 --brokerHost 10.0.69.1 --brokerPort 1883
